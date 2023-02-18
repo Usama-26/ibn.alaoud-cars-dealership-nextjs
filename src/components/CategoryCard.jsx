d569b4d990203a07e43f5ca8d56984f59c6ab695
Usama-26 <75754522+Usama-26@users.noreply.github.com>
2023-02-19 00:15:25 +0500

    name fixes

--- src/components/CategoryCard.jsx
+++ src/components/CategoryCard.jsx
@@ -5,7 +5,7 @@ export default function CategoryCard({ category }) {
     <div className="category-card border hover:border-gray-500 p-6 transition duration-200">
       <img
         src={`/images/cars/categories/${category}.png`}
-        alt={`Coupe`}
+        alt={category}
         className="mx-auto transition duration-300 ease-in-out object-cover h-16"
       />
       <h3 className="text-center font-bold capitalize mt-4 transition duration-200">
